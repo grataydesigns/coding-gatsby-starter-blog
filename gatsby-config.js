446ccc2e285820765f45602f3c4326c06d1f2509
Jonri <jonri.rothwell@ibm.com>
2020-12-09 18:44:36 -0500

    second commit

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+const siteMetadata = {
+	title: "The Localhost Blog",
+	description: "This is my coding blog where I write about my coding journey.",
+	image: `/static/code.jpg`,
+	siteUrl: `https://thelocalhost.blog`,
+	siteLanguage: "en-US",
+	siteLocale: `en_us`,
+	twitterUsername: "@grataydesigns",
+	authorName: `Jonri Rothwell`,
+};
+
+module.exports = {
+	siteMetadata,
+	plugins: [
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				extensions: [`.mdx`, `.md`],
+				gatsbyRemarkPlugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+						},
+					},
+				],
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+						},
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `posts`,
+				path: `${__dirname}/posts/`,
+			},
+		},
+	],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,80 @@
+import { graphql, Link } from "gatsby";
+import Img from "gatsby-image";
+import React from "react";
+import SEO from "react-seo-component";
+import styled from "styled-components";
+import { Layout } from "../components/Layout";
+import { useSiteMetadata } from "../hooks/useSiteMetadata";
+
+const PostWrapper = styled.div``;
+const Image = styled(Img)`
+	border-radius: 5px;
+`;
+
+export default ({ data }) => {
+	const {
+		description,
+		title,
+		image,
+		siteUrl,
+		siteLanguage,
+		siteLocale,
+		twitterUsername,
+	} = useSiteMetadata();
+	return (
+		<>
+			<Layout>
+				<SEO
+					title={title}
+					description={description}
+					image={`${siteUrl}${image}`}
+					pathname={siteUrl}
+					siteLanguage={siteLanguage}
+					siteLocale={siteLocale}
+					twitterUsername={twitterUsername}
+				/>
+				{data.allMdx.nodes.map(({ id, excerpt, frontmatter, fields }) => (
+					<PostWrapper key={id}>
+						<Link to={fields.slug}>
+							{!!frontmatter.cover ? (
+								<Image fluid={frontmatter.cover.childImageSharp.fluid} />
+							) : null}
+							<h1>{frontmatter.title}</h1>
+							<p>{frontmatter.date}</p>
+							<p>{excerpt}</p>
+						</Link>
+					</PostWrapper>
+				))}
+			</Layout>
+		</>
+	);
+};
+
+export const query = graphql`
+	query SITE_INDEX_QUERY {
+		allMdx(
+			sort: { fields: [frontmatter___date], order: DESC }
+			filter: { frontmatter: { published: { eq: true } } }
+		) {
+			nodes {
+				id
+				excerpt(pruneLength: 250)
+				frontmatter {
+					title
+					date(formatString: "YYYY MMM Do")
+					cover {
+						publicURL
+						childImageSharp {
+							fluid(maxWidth: 500, quality: 100, traceSVG: { color: "#639" }) {
+								...GatsbyImageSharpFluid_tracedSVG
+							}
+						}
+					}
+				}
+				fields {
+					slug
+				}
+			}
+		}
+	}
+`;
